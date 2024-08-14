e8bf41a35141912270ec9ef48269ce5b9c8a3570
jongmin <charlieko123@github.com>
2024-08-14 14:06:58 +0900

    :wrench:[Chore] module.css 적용

--- vms_fe/src/components/SearchBar.js
+++ vms_fe/src/components/SearchBar.js
@@ -1,13 +1,13 @@
 import searchIcon from "../assets/images/ic_search.svg";
 import deleteIcon from "../assets/images/ic_delete_circle_small.svg";
-import "./SearchBar.css";
+import styles from "./SearchBar.module.css";
 
 const SearchBar = ({ value, onChange, onClear }) => {
   return (
-    <div className="search-bar">
+    <div className={styles.searchBar}>
       <input
         type="text"
-        className="search-input"
+        className={styles.searchInput}
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder="검색어를 입력해주세요"
@@ -15,13 +15,15 @@ const SearchBar = ({ value, onChange, onClear }) => {
       <img
         src={searchIcon}
         alt="Search button"
-        className={`search-icon ${value ? "active" : ""}`}
+        className={`${styles.searchIcon} ${
+          value ? styles.searchIconActive : ""
+        }`}
       />
       {value && (
         <img
           src={deleteIcon}
           alt="Delete button"
-          className="delete-button"
+          className={styles.deleteButton}
           onClick={onClear}
         />
       )}
