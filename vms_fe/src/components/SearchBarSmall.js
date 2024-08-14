ffeb8930052ef88bc4263342b3a0aebceee16130
jongmin <charlieko123@github.com>
2024-08-14 14:28:32 +0900

    :sparkles:[Feat] SearchBarSmall.js 생성

---
+++ vms_fe/src/components/SearchBarSmall.js
@@ -0,0 +1,24 @@
+import searchIcon from "../assets/images/ic_search.svg";
+
+const SearchBarSmall = ({ value, onChange }) => {
+  return (
+    <div className={styles.searchBar}>
+      <input
+        type="text"
+        className={styles.searchInput}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="검색"
+      />
+      <img
+        src={searchIcon}
+        alt="Search button"
+        className={`${styles.searchIcon} ${
+          value ? styles.searchIconActive : ""
+        }`}
+      />
+    </div>
+  );
+};
+
+export default SearchBarSmall;
